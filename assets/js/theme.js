a7cb14a24a4ce867e2fa4ad74d9f8b6f2c1c78d3
Solomon Shalom Lijo <solomonlijo@skiff.com>
2025-03-13 00:12:30 +0530

    Jesus W!

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -13,9 +13,9 @@ const getColorPreference = () => {
   if (localStorage.getItem(storageKey))
     return localStorage.getItem(storageKey)
   else
-    return window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'dark'
-      : 'light'
+    return window.matchMedia('(prefers-color-scheme: light)').matches
+      ? 'light'
+      : 'dark'
 }
 
 const setPreference = () => {
